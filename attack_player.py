11d55bb17862198ac3a46ac8522752eb51f0981e
claire <1097457668@qq.com>
2021-11-16 13:01:41 +0100

    rajout les commantair

--- attack_player.py
+++ attack_player.py
@@ -1,5 +1,7 @@
 
 def attack_p (pv_ennemi,pa_player):
+     #This function takes away health points from enemy life's e when an player attacks him.
+     # if health points of enemy less then point attack of player, payer will win.
     if pv_ennemi <= pa_player:
         print("Filicitation,Vous avez réussi à vaincre l'ennemi!") 
         return "Filicitation,Vous avez réussi à vaincre l'ennemi!"
