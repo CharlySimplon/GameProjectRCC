db889d26423fbaaadfed761de4dbdcc40cb62599
CharlySimplon <93534235+CharlySimplon@users.noreply.github.com>
2021-11-17 10:04:44 +0100

    Delete potions.py

--- potions.py
+++
@@ -1,8 +0,0 @@
-def drink_potion(players_hp, max_players_hp, potion):
-    if players_hp < max_players_hp -20:
-        players_hp += 20
-    else:
-        players_hp = max_players_hp
-    potion -= 1
-    print(f'-------Vous avez maintenant {potion} potions et {players_hp}PV')
-    return players_hp, potion
