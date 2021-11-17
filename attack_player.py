8a27c44b047d79ffe0cd7ce8bef271a4423ebd51
CharlySimplon <93534235+CharlySimplon@users.noreply.github.com>
2021-11-17 10:04:09 +0100

    Delete attack_player.py

--- attack_player.py
+++
@@ -1,9 +0,0 @@
-
-def attack_p (hp_ennemy,attack_player):
-    if hp_ennemy <= attack_player:
-        print("Filicitation,Vous avez réussi à vaincre l'ennemi!") 
-        return 0
-    else:
-        print(f'Il reste {hp_ennemy} PV à l\'ennemi')
-        return hp_ennemy - attack_player
-    
